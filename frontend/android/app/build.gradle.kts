944d471baf419eb166bf4239d1710f5bc695334e
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-09-01 15:00:25 +0000

    chore(app): change package ID to dev.nelmatrix.keebox

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "dev.nelmatrix.keebox.keebox"
+    namespace = "dev.nelmatrix.keebox"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "dev.nelmatrix.keebox.keebox"
+        applicationId = "dev.nelmatrix.keebox"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- frontend/android/app/src/main/kotlin/dev/nelmatrix/keebox/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/dev/nelmatrix/keebox/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.nelmatrix.keebox.keebox
+package dev.nelmatrix.keebox
 
 import io.flutter.embedding.android.FlutterActivity
 
