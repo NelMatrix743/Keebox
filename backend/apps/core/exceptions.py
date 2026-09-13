3f86705ac046f86ab6c4cc510e28cb2117cf21e4
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-09-13 22:51:58 +0000

    feat(exception): define keebox key decryption error class

--- backend/apps/core/exceptions.py
+++ backend/apps/core/exceptions.py
@@ -1,2 +1,6 @@
 class EmailDeliveryError(Exception):
     """Indicate that an email could not be submitted for delivery."""
+
+
+class KeeboxKeyDecryptionError(Exception):
+    """Indicate that an encrypted KBKey could not be safely recovered."""
